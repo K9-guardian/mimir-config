d23779c9cbab81c5724f15c8c7b3821236ff2441
Krishna Girkar <girkar.km@gmail.com>
2026-06-07 22:19:24 -0700

    use direct output file with gluetun script

--- roles/podman/files/get-gluetun-forwarded-port.sh
+++ roles/podman/files/get-gluetun-forwarded-port.sh
@@ -3,12 +3,10 @@ GLUETUN_PORT="$1"
 ENV_KEY="$2"
 OUTPUT_FILE="$3"
 
-TARGET_DIR="${XDG_RUNTIME_DIR}"
-
 PORT=$(curl -s "http://localhost:${GLUETUN_PORT}/v1/portforward" | jq '.port')
 
 if [ "$PORT" -gt 0 ]; then
-  echo "${ENV_KEY}=${PORT}" > "${TARGET_DIR}/${OUTPUT_FILE}"
+  echo "${ENV_KEY}=${PORT}" > "${OUTPUT_FILE}"
   echo "Received port $PORT"
 else
   echo "Port $PORT is invalid" >&2
