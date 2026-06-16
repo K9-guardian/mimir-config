229ebed24e5315b6f93c92798ae78722a33a014a
Krishna Girkar <girkar.km@gmail.com>
2026-06-16 11:26:19 -0700

    setup tailscale

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 ANSIBLE_VENV="/opt/ansible-venv"
 sudo python3 -m venv --system-site-packages "$ANSIBLE_VENV"
 sudo "$ANSIBLE_VENV/bin/pip" install ansible==13.6.0
+"$ANSIBLE_VENV/bin/ansible-galaxy" collection install -r requirements.yml
